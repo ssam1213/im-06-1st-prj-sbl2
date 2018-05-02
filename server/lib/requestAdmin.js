3a687f7cf6931612976106a41634d1825666165f
BackYoungJae <byj010101@gmail.com>
2018-05-02 14:23:13 +0900

    react - server

--- server/lib/requestAdmin.js
+++ server/lib/requestAdmin.js
@@ -3,52 +3,53 @@ var adminModels = require('../models/indexAdmin');
 module.exports.mid = function (req, res) {
     console.log('mid model activated')
     var data1 = {
-        labels: ['04', '08', '12', '16', '20', '24'],
+        labels: ['00', '01', '02', '03', '04', '05', '00', '01', '02', '03', '04', '05', '00', '01', '02', '03', '04', '05', '00', '01', '02', '03', '04', '05' ],
+        // labels: ['04', '08', '12', '16', '20', '24'],
         datasets: [
         {
             label: 'visit',
-            data: [],
+                data: [10, 20, 30, 40, 50, 10, 20, 30, 40, 50, 10, 20, 30, 40, 50, 10, 20, 30, 40, 50, 10, 20, 30, 40],
             backgroundColor: ['#FF6384']
         },
         {
-            labal: 'revenue',
-            data: [],
+            label: 'revenue',
+            data: [100, 200, 300, 400, 100, 200, 300, 400, 100, 200, 300, 400, 100, 200, 300, 400, 100, 200, 300, 400, 100, 200, 300, 400],
             borderColor: ['#FFCE56']
         }
         ]
     }
-    var time = ["'2018-05-01 00:00:00' and '2018-05-01 03:59:59';", 
-                "'2018-05-01 04:00:00' and '2018-05-01 07:59:59';", 
-                "'2018-05-01 08:00:00' and '2018-05-01 11:59:59';", 
-                "'2018-05-01 12:00:00' and '2018-05-01 15:59:59';", 
-                "'2018-05-01 16:00:00' and '2018-05-01 19:59:59';", 
-                "'2018-05-01 20:00:00' and '2018-05-01 23:59:59';"];
-    var queryRevenue = 'SELECT sum(id) FROM visitors WHERE visitorTime BETWEEN';
-    var queryVisitors = 'SELECT sum(id) FROM visitors WHERE visitorTime BETWEEN ';
-    var queryRevenue = 'SELECT sum(price) FROM revenue WHERE revenueTime BETWEEN ';
-    var queryVisitors = 'SELECT count(token) FROM visitors WHERE visitorTime BETWEEN ';
+    // var time = ["'2018-05-01 00:00:00' and '2018-05-01 03:59:59';", 
+    //             "'2018-05-01 04:00:00' and '2018-05-01 07:59:59';", 
+    //             "'2018-05-01 08:00:00' and '2018-05-01 11:59:59';", 
+    //             "'2018-05-01 12:00:00' and '2018-05-01 15:59:59';", 
+    //             "'2018-05-01 16:00:00' and '2018-05-01 19:59:59';", 
+    //             "'2018-05-01 20:00:00' and '2018-05-01 23:59:59';"];
+    // var queryRevenue = 'SELECT sum(id) FROM visitors WHERE visitorTime BETWEEN';
+    // var queryVisitors = 'SELECT sum(id) FROM visitors WHERE visitorTime BETWEEN ';
+    // var queryRevenue = 'SELECT sum(price) FROM revenue WHERE revenueTime BETWEEN ';
+    // var queryVisitors = 'SELECT count(token) FROM visitors WHERE visitorTime BETWEEN ';
 
-    for (var i = 0; i < time.length; i++) {
-        var query = queryRevenue + time[i];
-        console.log('query : ', query);
-        adminModels.mid(query, function (err, rows) {
-            if (err) {
-                throw err;
-            } else {
-                data1.dataset[1].data.push(rows);
-            }
-        })
-        var query = queryVisitors + time[i];
-        console.log('query : ', query);
-        adminModels.mid(query, function (err, rows) {
-            if (err) {
-                throw err;
-            } else {
-                data1.dataset[0].data.push(rows);
-            }
-        })
-    }
-    console.log('data1 : ', data1);
+    // for (var i = 0; i < time.length; i++) {
+    //     var query = queryRevenue + time[i];
+    //     console.log('query : ', query);
+    //     adminModels.mid(query, function (err, rows) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             data1.dataset[1].data.push(rows);
+    //         }
+    //     })
+    //     var query = queryVisitors + time[i];
+    //     console.log('query : ', query);
+    //     adminModels.mid(query, function (err, rows) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             data1.dataset[0].data.push(rows);
+    //         }
+    //     })
+    // }
+    // console.log('data1 : ', data1);
     res.send(JSON.stringify(data1));
 }
 
@@ -72,74 +73,74 @@ module.exports.left = function (req, res) {
         },
     ];
 
-    var time = ["'2018-05-01 00:00:00' and '2018-05-01 03:59:59';", 
-                "'2018-05-01 04:00:00' and '2018-05-01 07:59:59';", 
-                "'2018-05-01 08:00:00' and '2018-05-01 11:59:59';", 
-                "'2018-05-01 12:00:00' and '2018-05-01 15:59:59';", 
-                "'2018-05-01 16:00:00' and '2018-05-01 19:59:59';", 
-                "'2018-05-01 20:00:00' and '2018-05-01 23:59:59';"];
-    var queryRevenue = 'SELECT sum(price) FROM revenue WHERE revenueTime BETWEEN ';
-    var queryPageviews = 'SELECT count(pageName) FROM pageviews WHERE pageTime BETWEEN ';
-    var queryVisitors = 'SELECT count(token) FROM visitors WHERE visitorTime BETWEEN ';
+    // var time = ["'2018-05-01 00:00:00' and '2018-05-01 03:59:59';", 
+    //             "'2018-05-01 04:00:00' and '2018-05-01 07:59:59';", 
+    //             "'2018-05-01 08:00:00' and '2018-05-01 11:59:59';", 
+    //             "'2018-05-01 12:00:00' and '2018-05-01 15:59:59';", 
+    //             "'2018-05-01 16:00:00' and '2018-05-01 19:59:59';", 
+    //             "'2018-05-01 20:00:00' and '2018-05-01 23:59:59';"];
+    // var queryRevenue = 'SELECT sum(price) FROM revenue WHERE revenueTime BETWEEN ';
+    // var queryPageviews = 'SELECT count(pageName) FROM pageviews WHERE pageTime BETWEEN ';
+    // var queryVisitors = 'SELECT count(token) FROM visitors WHERE visitorTime BETWEEN ';
 
-    var queryRe = 'SELECT sum(price) FROM revenue;';
-    var queryPa = 'SELECT count(pageName) FROM pageviews;';
-    var queryVi = 'SELECT count(token) FROM visitors;';
+    // var queryRe = 'SELECT sum(price) FROM revenue;';
+    // var queryPa = 'SELECT count(pageName) FROM pageviews;';
+    // var queryVi = 'SELECT count(token) FROM visitors;';
 
-    adminModels.left(queryRe, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            stats[0].current = rows;
-        }
-    })
+    // adminModels.left(queryRe, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         stats[0].current = rows;
+    //     }
+    // })
 
-    adminModels.left(queryPa, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            stats[1].current = rows;
-        }
-    })
+    // adminModels.left(queryPa, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         stats[1].current = rows;
+    //     }
+    // })
 
-    adminModels.left(queryVi, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            stats[2].current = rows;
-        }
-    })
+    // adminModels.left(queryVi, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         stats[2].current = rows;
+    //     }
+    // })
 
-    for (var i = 0; i < time.length; i++) {
-        var query = queryRevenue + time[i];
-        console.log('query : ', query);
-        adminModels.left(query, function (err, rows) {
-            if (err) {
-                throw err;
-            } else {
-                stats[0].timeSeries.push(rows);
-            }
-        })
-        var query = queryPageviews + time[i];
-        console.log('query : ', query);
-        adminModels.left(query, function (err, rows) {
-            if (err) {
-                throw err;
-            } else {
-                stats[1].timeSeries.push(rows);
-            }
-        })
-        var query = queryVisitors + time[i];
-        console.log('query : ', query);
-        adminModels.left(query, function (err, rows) {
-            if (err) {
-                throw err;
-            } else {
-                stats[2].timeSeries.push(rows);
-            }
-        })
-    }
-    console.log('stats : ', stats);
+    // for (var i = 0; i < time.length; i++) {
+    //     var query = queryRevenue + time[i];
+    //     console.log('query : ', query);
+    //     adminModels.left(query, function (err, rows) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             stats[0].timeSeries.push(rows);
+    //         }
+    //     })
+    //     var query = queryPageviews + time[i];
+    //     console.log('query : ', query);
+    //     adminModels.left(query, function (err, rows) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             stats[1].timeSeries.push(rows);
+    //         }
+    //     })
+    //     var query = queryVisitors + time[i];
+    //     console.log('query : ', query);
+    //     adminModels.left(query, function (err, rows) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             stats[2].timeSeries.push(rows);
+    //         }
+    //     })
+    // }
+    // console.log('stats : ', stats);
     res.send(JSON.stringify(stats));
 }
 
@@ -148,8 +149,8 @@ module.exports.right = function (req, res) {
     var result = {
         itemData: [
             { id: 1, name: '', price: 0, visit: 0 },
-            { id: 3, name: '', price: 0, visit: 0 },
             { id: 2, name: '', price: 0, visit: 0 },
+            { id: 3, name: '', price: 0, visit: 0 },
             { id: 4, name: '', price: 0, visit: 0 }
         ],
         pagaData: [
@@ -161,49 +162,49 @@ module.exports.right = function (req, res) {
     }
    
 
-    var item = '';
-    var queryItem = 'SELECT itemName, count(itemName) AS pageView FROM pageviews GROUP BY pageName ORDER BY count(pageName) DESC;';
-    adminModels.right(queryItem, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            item = rows[0];
-        }
-    })
-    var queryItemPrice = `SELECT sum(price) FROM revenue WHERE itemName = '${item}'`;
-    adminModels.right(queryItemPrice, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            result.itemData[0].price = rows;
-        }
-    })
-    var queryItemVisits = `SELECT count(token) FROM visitors LEFT JOIN pageviews ON pageviews.visitors_id = visitors.id LEFT JOIN revenue ON revenue.pageviews_id = pageviews.id WHERE itemName = '${item}';`;
-    adminModels.right(queryItemVisits, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            result.itemData[0].visit = rows;
-        }
-    })
+    // var item = '';
+    // var queryItem = 'SELECT itemName, count(itemName) AS pageView FROM pageviews GROUP BY pageName ORDER BY count(pageName) DESC;';
+    // adminModels.right(queryItem, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         item = rows[0];
+    //     }
+    // })
+    // var queryItemPrice = `SELECT sum(price) FROM revenue WHERE itemName = '${item}'`;
+    // adminModels.right(queryItemPrice, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         result.itemData[0].price = rows;
+    //     }
+    // })
+    // var queryItemVisits = `SELECT count(token) FROM visitors LEFT JOIN pageviews ON pageviews.visitors_id = visitors.id LEFT JOIN revenue ON revenue.pageviews_id = pageviews.id WHERE itemName = '${item}';`;
+    // adminModels.right(queryItemVisits, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         result.itemData[0].visit = rows;
+    //     }
+    // })
 
-    var page = '';
-    var queryPage = 'SELECT pageName, count(pageName) AS pageView FROM pageviews GROUP BY pageName ORDER BY count(pageName) DESC;';
-    adminModels.right(queryPage, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            page = rows[0];
-        }
-    })
-    var queryPageview = `SELECT count(pageTime) FROM pageviews WHERE pageName = '${page}';`;
-    adminModels.right(queryPageview, function (err, rows) {
-        if (err) {
-            throw err;
-        } else {
-            result.pageData[0].pageview = rows;
-        }
-    })
+    // var page = '';
+    // var queryPage = 'SELECT pageName, count(pageName) AS pageView FROM pageviews GROUP BY pageName ORDER BY count(pageName) DESC;';
+    // adminModels.right(queryPage, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         page = rows[0];
+    //     }
+    // })
+    // var queryPageview = `SELECT count(pageTime) FROM pageviews WHERE pageName = '${page}';`;
+    // adminModels.right(queryPageview, function (err, rows) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         result.pageData[0].pageview = rows;
+    //     }
+    // })
     res.send(JSON.stringify(result))
 }
 
